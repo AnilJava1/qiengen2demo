d6beac22b6b5397bc75956ff4ae931d378e9afce
AnilJava1 <amahabali@hotmail.com>
2018-07-02 11:25:06 +0200

    eerste commit

---
+++ Qien.java
@@ -0,0 +1,18 @@
+class Qien{
+	public static void main(String[] args){
+		System.out.println("Laten we kijken wat je mag");
+		int leeftijd = 19;
+		
+		
+		if(leeftijd < 16){
+			System.out.println("Helemaal niks");
+		}
+		if(leeftijd >= 16 && leeftijd < 18){
+			System.out.println("Alleen bier");
+		}
+		if(leeftijd > 18){
+			System.out.println("Alles mag");
+		}
+	}
+
+}
